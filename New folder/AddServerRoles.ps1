ce4cc23ba8ec245e7c5b1e68d01f3ea5bbb2dc5f
Pratik Thakkar <thakkarpratik277@gmail.com>
2018-06-23 07:43:31 +0530

    Update AddServerRoles.ps1

--- New folder/AddServerRoles.ps1
+++ New folder/AddServerRoles.ps1
@@ -1,2 +1,2 @@
-$ServerFeatures = Import-Clixml \\sgdc01\DevOps\ServerFeatures_IDX.xml
+$ServerFeatures = Import-Clixml \\DevOps\ServerFeatures_IDX.xml
 foreach ($feature in $ServerFeatures) {Install-WindowsFeature -Name $feature.name}
